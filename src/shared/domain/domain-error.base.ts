22d55764dc0acef0502603b6f6488e258db64680
TENKORANG GODFRED <godtenkorang72@gmail.com>
2026-07-03 01:09:01 +0000

    Shared: add DomainError base for invariant violations

---
+++ src/shared/domain/domain-error.base.ts
@@ -0,0 +1,10 @@
+export abstract class DomainError extends Error {
+  public readonly code: string;
+
+  constructor(message: string, code: string) {
+    super(message);
+    this.code = code;
+    this.name = this.constructor.name;
+  }
+}
+
