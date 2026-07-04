0a364f0b99a278df251b01a05c2f5abebf58e9d5
TENKORANG GODFRED <godtenkorang72@gmail.com>
2026-07-04 01:14:12 +0000

    Users: add DTOs for invite, accept, profile, and password change

---
+++ src/users/infrastructure/http/dto/invite-user.dto.ts
@@ -0,0 +1,48 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsEnum, IsString, MinLength } from 'class-validator';
+import { UserRole } from '@prisma/client';
+
+export class InviteUserDto {
+  @ApiProperty({ example: 'Kwame Mensah' })
+  @IsString()
+  @MinLength(2)
+  name!: string;
+
+  @ApiProperty({ example: 'kwame@veritrack.cloud' })
+  @IsEmail()
+  email!: string;
+
+  @ApiProperty({ enum: UserRole, example: UserRole.DEVELOPER })
+  @IsEnum(UserRole)
+  role!: UserRole;
+}
+
+export class AcceptInviteDto {
+  @ApiProperty()
+  @IsString()
+  @MinLength(20)
+  token!: string;
+
+  @ApiProperty({ minLength: 8 })
+  @IsString()
+  @MinLength(8)
+  password!: string;
+}
+
+export class UpdateMeDto {
+  @ApiProperty()
+  @IsString()
+  @MinLength(2)
+  name!: string;
+}
+
+export class ChangePasswordDto {
+  @ApiProperty()
+  @IsString()
+  currentPassword!: string;
+
+  @ApiProperty({ minLength: 8 })
+  @IsString()
+  @MinLength(8)
+  newPassword!: string;
+}
