b4e255f661d79e31e4d932a052fed1ed473bb503
TENKORANG GODFRED <godtenkorang72@gmail.com>
2026-07-04 01:15:56 +0000

    Portals: wire portal repo, use cases, and admin controller

---
+++ src/portals/portals.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { PORTAL_REPOSITORY_PORT } from './application/ports/portal-repository.port';
+import { PrismaPortalRepository } from './infrastructure/persistence/prisma-portal.repository';
+import { RegisterPortalUseCase } from './application/use-cases/register-portal/register-portal.use-case';
+import { ListPortalsUseCase } from './application/use-cases/list-portals/list-portals.use-case';
+import { RotatePortalKeyUseCase } from './application/use-cases/rotate-portal-key/rotate-portal-key.use-case';
+import { UpdatePortalStatusUseCase } from './application/use-cases/update-portal-status/update-portal-status.use-case';
+import { PortalsController } from './infrastructure/http/portals.controller';
+
+@Module({
+  controllers: [PortalsController],
+  providers: [
+    { provide: PORTAL_REPOSITORY_PORT, useClass: PrismaPortalRepository },
+    RegisterPortalUseCase,
+    ListPortalsUseCase,
+    RotatePortalKeyUseCase,
+    UpdatePortalStatusUseCase,
+    PrismaPortalRepository,
+  ],
+  exports: [PORTAL_REPOSITORY_PORT, PrismaPortalRepository],
+})
+export class PortalsModule {}
