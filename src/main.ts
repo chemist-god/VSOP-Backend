6ad640a48c8d12aca2468b72d73734f4773c8d80
TENKORANG GODFRED <godtenkorang72@gmail.com>
2026-07-07 13:36:13 +0000

    App: bootstrap NestJS with helmet, CORS, validation, Swagger

---
+++ src/main.ts
@@ -0,0 +1,45 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
+import { AppModule } from './app.module';
+import { DomainExceptionFilter } from './shared/infrastructure/filters/domain-exception.filter';
+
+async function bootstrap(): Promise<void> {
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
+  app.use(helmet());
+  app.enableCors({
+    origin: configService.get<string[]>('security.corsOrigins') ?? ['http://localhost:3000'],
+    credentials: true,
+  });
+
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  app.useGlobalFilters(new DomainExceptionFilter());
+
+  if (configService.get<string>('app.nodeEnv') !== 'production') {
+    const swaggerConfig = new DocumentBuilder()
+      .setTitle('VSOP API')
+      .setDescription('VeriTrack Support Operations Platform API')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .build();
+
+    const document = SwaggerModule.createDocument(app, swaggerConfig);
+    SwaggerModule.setup('api/docs', app, document);
+  }
+
+  const port = configService.get<number>('app.port') ?? 3001;
+  await app.listen(port);
+}
+
+void bootstrap();
