987e6fa1c8fbdde99672c752a9517f6805bf28bd
TENKORANG GODFRED <godtenkorang72@gmail.com>
2026-07-16 16:26:53 +0000

    Portals: register UpdatePortalUseCase

--- src/portals/portals.module.ts
+++ src/portals/portals.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { PORTAL_REPOSITORY_PORT } from './application/ports/portal-repository.port';
 import { PrismaPortalRepository } from './infrastructure/persistence/prisma-portal.repository';
 import { RegisterPortalUseCase } from './application/use-cases/register-portal/register-portal.use-case';
+import { UpdatePortalUseCase } from './application/use-cases/update-portal/update-portal.use-case';
 import { ListPortalsUseCase } from './application/use-cases/list-portals/list-portals.use-case';
 import { RotatePortalKeyUseCase } from './application/use-cases/rotate-portal-key/rotate-portal-key.use-case';
 import { UpdatePortalStatusUseCase } from './application/use-cases/update-portal-status/update-portal-status.use-case';
@@ -12,6 +13,7 @@ import { PortalsController } from './infrastructure/http/portals.controller';
   providers: [
     { provide: PORTAL_REPOSITORY_PORT, useClass: PrismaPortalRepository },
     RegisterPortalUseCase,
+    UpdatePortalUseCase,
     ListPortalsUseCase,
     RotatePortalKeyUseCase,
     UpdatePortalStatusUseCase,
